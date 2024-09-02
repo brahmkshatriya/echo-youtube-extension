444253f181db184b61538fecfc15a3ff639d21b9
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-09-02 06:33:35 +0530

    update build.gradle.kts, to now support compiling jars too

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,8 @@
-import com.android.build.gradle.AppExtension
 import java.io.ByteArrayOutputStream
 
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-}
-
-val gitHash = execute("git", "rev-parse", "HEAD").take(7)
-val gitCount = execute("git", "rev-list", "--count", "HEAD").toInt()
-
-apply<EchoExtensionPlugin>()
-configure<EchoExtension> {
-    versionCode = gitCount
-    versionName = gitHash
-    extensionClass = "YoutubeExtension"
-    id = "youtube-music"
-    name = "Youtube Music"
-    description = "Youtube Music Extension for Echo, with the help of YTM-kt library."
-    author = "Echo"
-    iconUrl = "https://music.youtube.com/img/favicon_144.png"
 }
 
 dependencies {
@@ -30,13 +14,66 @@ dependencies {
     compileOnly("com.github.brahmkshatriya:echo:$libVersion")
 }
 
+val extType: String by project
+val extId: String by project
+val extClass: String by project
+
+val extIconUrl: String? by project
+val extName: String by project
+val extDescription: String? by project
+
+val extAuthor: String by project
+val extAuthorUrl: String? by project
+
+val extRepoUrl: String? by project
+val extUpdateUrl: String? by project
+
+val gitHash = execute("git", "rev-parse", "HEAD").take(7)
+val gitCount = execute("git", "rev-list", "--count", "HEAD").toInt()
+val verCode = gitCount
+val verName = gitHash
+
 android {
     namespace = "dev.brahmkshatriya.echo.extension"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "dev.brahmkshatriya.echo.extension.ytm"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
+
+        manifestPlaceholders.apply {
+            put("type", "dev.brahmkshatriya.echo.${extType}")
+            put("id", extId)
+            put("class_path", "dev.brahmkshatriya.echo.extension.${extClass}")
+            put("version", verName)
+            put("version_code", verCode.toString())
+            extIconUrl?.let { put("icon_url", it) }
+            put("app_name", "Echo : $extName Extension")
+            put("name", extName)
+            extDescription?.let { put("description", it) }
+            put("author", extAuthor)
+            extAuthorUrl?.let { put("author_url", it) }
+            extRepoUrl?.let { put("repo_url", it) }
+            extUpdateUrl?.let { put("update_url", it) }
+        }
+        resValue("string", "id", extId)
+        resValue("string", "class_path", "$namespace.${extClass}")
+
+        versionName = verName
+        resValue("string", "version", verName)
+        versionCode = verCode
+        resValue("string", "version_code", verCode.toString())
+
+        extIconUrl?.let { resValue("string", "icon_url", it) }
+        resValue("string", "app_name", "Echo : $extName Extension")
+        resValue("string", "name", extName)
+        description?.let { resValue("string", "description", it) }
+
+        resValue("string", "author", extAuthor)
+        extAuthorUrl?.let { resValue("string", "author_url", it) }
+
+        extRepoUrl?.let { resValue("string", "repo_url", it) }
+        extUpdateUrl?.let { resValue("string", "update_url", it) }
     }
 
     buildTypes {
@@ -52,40 +89,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-open class EchoExtension {
-    var extensionClass: String? = null
-    var id: String? = null
-    var name: String? = null
-    var description: String? = null
-    var author: String? = null
-    var iconUrl: String? = null
-    var versionCode: Int? = null
-    var versionName: String? = null
-}
-
-abstract class EchoExtensionPlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        val echoExtension = project.extensions.create("echoExtension", EchoExtension::class.java)
-        project.afterEvaluate {
-            project.extensions.configure<AppExtension>("android") {
-                defaultConfig.apply {
-                    with(echoExtension) {
-                        resValue("string", "id", id!!)
-                        resValue("string", "name", name!!)
-                        resValue("string", "app_name", "Echo : $name Extension")
-                        val extensionClass = extensionClass!!
-                        resValue("string", "class_path", "$namespace.$extensionClass")
-                        resValue("string", "version", versionName!!)
-                        resValue("string", "description", description!!)
-                        resValue("string", "author", author!!)
-                        iconUrl?.let { resValue("string", "icon_url", it) }
-                    }
-                }
-            }
-        }
     }
 }
 
--- ext/build.gradle.kts
+++ ext/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.io.ByteArrayOutputStream
 
 plugins {
     id("java-library")
@@ -31,8 +32,59 @@ dependencies {
     testImplementation("com.github.brahmkshatriya:echo:$libVersion")
 }
 
+val extType: String by project
+val extId: String by project
+val extClass: String by project
+
+val extIconUrl: String? by project
+val extName: String by project
+val extDescription: String? by project
+
+val extAuthor: String by project
+val extAuthorUrl: String? by project
+
+val extRepoUrl: String? by project
+val extUpdateUrl: String? by project
+
+val gitHash = execute("git", "rev-parse", "HEAD").take(7)
+val gitCount = execute("git", "rev-list", "--count", "HEAD").toInt()
+val verCode = gitCount
+val verName = gitHash
+
 tasks {
     val shadowJar by getting(ShadowJar::class) {
-        archiveBaseName.set("ytm")
+        archiveBaseName.set(extId)
+        archiveVersion.set(verName)
+        manifest {
+            attributes(
+                mapOf(
+                    "Extension-Id" to extId,
+                    "Extension-Type" to extType,
+                    "Extension-Class" to extClass,
+
+                    "Extension-Version-Code" to verCode,
+                    "Extension-Version-Name" to verName,
+
+                    "Extension-Icon-Url" to extIconUrl,
+                    "Extension-Name" to extName,
+                    "Extension-Description" to extDescription,
+
+                    "Extension-Author" to extAuthor,
+                    "Extension-Author-Url" to extAuthorUrl,
+
+                    "Extension-Repo-Url" to extRepoUrl,
+                    "Extension-Update-Url" to extUpdateUrl
+                )
+            )
+        }
     }
+}
+
+fun execute(vararg command: String): String {
+    val outputStream = ByteArrayOutputStream()
+    project.exec {
+        commandLine(*command)
+        standardOutput = outputStream
+    }
+    return outputStream.toString().trim()
 }
